9bea8329cd68410fc3b202fb1c7b283dd0683ab5
Lucia Rivero <claudialucia.rivero@gmail.com>
2018-11-28 11:25:59 -0300

    build

---
+++ build/precache-manifest.b57c9e972927614783b2167797d0b015.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a1f2303659a4cdb9b55",
+    "url": "/static/css/main.1e566ef4.chunk.css"
+  },
+  {
+    "revision": "7a1f2303659a4cdb9b55",
+    "url": "/static/js/main.7a1f2303.chunk.js"
+  },
+  {
+    "revision": "5e599bcf4900aaf4206a",
+    "url": "/static/js/1.5e599bcf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "899230849afb5d5613c15ac61a7f0a19",
+    "url": "/index.html"
+  }
+];
